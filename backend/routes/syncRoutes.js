008ceb4fb6ee64f0fba07f050f5a95b5d449ac80
ranbirdevstack <ranbirpratapsinghbhatia@gmail.com>
2026-09-15 00:36:15 +0530

    Fix

--- backend/routes/syncRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/syncRoutes.js
-const express = require("express");
-const router = express.Router();
-const { processSync } = require("../controllers/syncController");
-const { protect } = require("../middleware/authMiddleware");
-
-router.route("/").post(protect, processSync);
-
-module.exports = router;
